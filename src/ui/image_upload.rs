022bc0b3751098e72ab2143eca584ca4315eb8a4
fralonra <fralonra@aliyun.com>
2023-07-14 17:45:51 +0800

    refactor image_upload

--- src/ui/image_upload.rs
+++ src/ui/image_upload.rs
@@ -3,18 +3,21 @@ use egui::{vec2, Color32, CursorIcon, Image, Response, Sense, Stroke, TextureHan
 pub fn image_upload(ui: &mut Ui, size: f32, texture: Option<&TextureHandle>) -> Response {
     let response = ui.allocate_response(Vec2::splat(size), Sense::click());
 
-    let stroke = Stroke::new(1.0, Color32::from_gray(128));
     let rect = response.rect;
-    let center = rect.center();
-    let half_len = size * 0.3;
+
+    let stroke = Stroke::new(1.0, Color32::from_gray(128));
 
     let painter = ui.painter_at(rect);
+
     match texture {
         Some(texture) => {
             let image = Image::new(texture, rect.size());
             image.paint_at(ui, rect);
         }
         None => {
+            let center = rect.center();
+            let half_len = size * 0.3;
+
             painter.line_segment(
                 [center - vec2(0.0, half_len), center + vec2(0.0, half_len)],
                 stroke,
@@ -23,13 +26,14 @@ pub fn image_upload(ui: &mut Ui, size: f32, texture: Option<&TextureHandle>) -> 
                 [center - vec2(half_len, 0.0), center + vec2(half_len, 0.0)],
                 stroke,
             );
+
+            if response.hovered() {
+                ui.output_mut(|o| o.cursor_icon = CursorIcon::PointingHand);
+            }
         }
     }
-    painter.rect_stroke(rect, 5.0, stroke);
 
-    if response.hovered() && texture.is_none() {
-        ui.output_mut(|o| o.cursor_icon = CursorIcon::PointingHand);
-    }
+    painter.rect_stroke(rect, 5.0, stroke);
 
     response
 }
