dfdb3e48c70bb1f53e1718f9969d2b3f04b8f830
fralonra <fralonra@aliyun.com>
2023-08-11 11:16:33 +0800

    use async file dialog

--- src/fs.rs
+++ src/fs.rs
@@ -1,29 +1,41 @@
-use rfd::FileDialog;
+use rfd::AsyncFileDialog;
 use std::{
     fs::write,
     path::{Path, PathBuf},
 };
 
 pub fn create_file(filename: &str) -> Option<PathBuf> {
-    FileDialog::new()
-        .set_directory("~")
-        .add_filter("WebGPU Shader", &[wgs_core::EXTENSION])
-        .set_file_name(filename)
-        .save_file()
+    futures::executor::block_on(async {
+        AsyncFileDialog::new()
+            .set_directory("~")
+            .add_filter("WebGPU Shader", &[wgs_core::EXTENSION])
+            .set_file_name(filename)
+            .save_file()
+            .await
+    })
+    .map(|file| file.path().to_path_buf())
 }
 
 pub fn select_file() -> Option<PathBuf> {
-    FileDialog::new()
-        .set_directory("~")
-        .add_filter("WebGPU Shader", &[wgs_core::EXTENSION])
-        .pick_file()
+    futures::executor::block_on(async {
+        AsyncFileDialog::new()
+            .set_directory("~")
+            .add_filter("WebGPU Shader", &[wgs_core::EXTENSION])
+            .pick_file()
+            .await
+    })
+    .map(|file| file.path().to_path_buf())
 }
 
 pub fn select_texture() -> Option<PathBuf> {
-    FileDialog::new()
-        .set_directory("~")
-        .add_filter("Textures", &["png", "jpg"])
-        .pick_file()
+    futures::executor::block_on(async {
+        AsyncFileDialog::new()
+            .set_directory("~")
+            .add_filter("Textures", &["png", "jpg"])
+            .pick_file()
+            .await
+    })
+    .map(|file| file.path().to_path_buf())
 }
 
 pub fn write_file<P, C>(path: P, contents: C)
